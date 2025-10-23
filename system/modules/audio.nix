55a56fe6575db715b48bdd1a8f7016c299afb4e2
ern775 <eren.demir2479090@gmail.com>
2025-10-23 17:15:59 +0300

    Audio fix

--- system/modules/audio.nix
+++ system/modules/audio.nix
@@ -1,15 +1,17 @@
-{ pkgs, ... }:
+{ ... }:
 {
+  security.rtkit = {
+    enable = true;
+  };
   services = {
     pipewire = {
-      # audio.enable = true;
       enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = true;
       extraConfig.pipewire = {
-        "10-clock-rate" = {
+        "clock-rate" = {
           "context.properties" = {
             "default.clock.rate" = 48000;
             "default.clock.allowed-rates" = [
@@ -18,61 +20,130 @@
               88200
               96000
             ];
-            "default.clock.min-quantum" = 1024;
-            "default.clock.max-quantum" = 8192;
-            "default.clock.quantum" = 1024;
+            "default.clock.min-quantum" = 256;
+            "default.clock.max-quantum" = 1024;
+            "default.clock.quantum" = 512;
             "default.clock.quantum-limit" = 8192;
-            "default.clock.quantum-floor" = 256;
+            "default.clock.quantum-floor" = 128;
           };
         };
+        # "98-disable-suspension" = {
+        #   "context.properties" = {
+        #     "session.suspend-timeout-seconds" = 0; # Prevent autosuspend of ALSA nodes, causing xruns and crashes
+        #   };
+        # };
+        "q10-parametric-equalizer" = {
+          "context.modules" = [
+            {
+              name = "libpipewire-module-parametric-equalizer";
+              args = {
+                "equalizer.filepath" = builtins.toString equalizers/Q10_ParametricEQ.txt;
+                "equalizer.description" = "Q10 Parametric EQ Sink";
+                "capture.props" = {
+                  "node.name" = "Q10 Parametric EQ Input";
+                  "filter.smart" = true;
+                  "filter.smart.name" = "q10-peq";
+                  "filter.smart.target" = {
+                    "node.name" = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink";
+                  };
+                };
+                "playback.props" = {
+                  "node.name" = "Q10 Parametric EQ Output";
+                };
+              };
+            }
+          ];
+        };
+        "SpaceTravel-parametric-equalizer" = {
+          "context.modules" = [
+            {
+              name = "libpipewire-module-parametric-equalizer";
+              args = {
+                "equalizer.filepath" = builtins.toString equalizers/SpaceTravel_ParametricEQ.txt;
+                "equalizer.description" = "SpaceTravel Parametric EQ Sink";
+                "capture.props" = {
+                  "node.name" = "SpaceTravel Parametric EQ Input";
+                  "filter.smart" = true;
+                  "filter.smart.name" = "SpaceTravel-peq";
+                  "filter.smart.target" = {
+                    "node.name" = "bluez_output.24_09_12_B3_35_A8.1";
+                  };
+                };
+                "playback.props" = {
+                  "node.name" = "SpaceTravel Parametric EQ Output";
+                };
+              };
+            }
+          ];
+        };
       };
+      # extraConfig.pipewire-pulse = {
+      #   "99-disable-suspension" = {
+      #     "pulse.properties" = {
+      #       "pulse.idle.timeout" = 0;
+      #     };
+      #   };
+      # };
       wireplumber = {
         enable = true;
 
         # source: https://wiki.archlinux.org/title/PipeWire#Noticeable_audio_delay_or_audible_pop/crack_when_starting_playback
-        configPackages = [
-          (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/51-disable-suspension.conf" ''
-            monitor.alsa.rules = [
+        extraConfig = {
+          "51-disable-suspension" = {
+            "monitor.alsa.rules" = [
               {
                 matches = [
                   {
                     # Matches all sources
-                    node.name = "~alsa_input.*"
-                  },
+                    "node.name" = "~alsa_input.*";
+                  }
                   {
                     # Matches all sinks
-                    node.name = "~alsa_output.*"
+                    "node.name" = "~alsa_output.*";
                   }
-                ]
-                actions = {
-                  update-props = {
-                    session.suspend-timeout-seconds = 0
-                  }
-                }
+                ];
+                actions.update-props = {
+                  "session.suspend-timeout-seconds" = 0;
+                };
               }
-            ]
+            ];
             # bluetooth devices
-            monitor.bluez.rules = [
+            "monitor.bluez.rules" = [
               {
                 matches = [
                   {
                     # Matches all sources
-                    node.name = "~bluez_input.*"
-                  },
+                    "node.name" = "~bluez_input.*";
+                  }
                   {
                     # Matches all sinks
-                    node.name = "~bluez_output.*"
+                    "node.name" = "~bluez_output.*";
                   }
-                ]
-                actions = {
-                  update-props = {
-                    session.suspend-timeout-seconds = 0
-                  }
-                }
+                ];
+                actions.update-props = {
+                  "session.suspend-timeout-seconds" = 0;
+                };
               }
-            ]
-          '')
-        ];
+            ];
+          };
+          "99-disable-unused-nodes" = {
+            "monitor.alsa.rules" = [
+              {
+                matches = [
+                  {
+                    "node.name" = "~alsa_output.*HDMI*";
+                  }
+                  {
+                    "node.name" = "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic2__source";
+                  }
+                ];
+                actions.update-props = {
+                  "node.disabled" = true;
+                };
+              }
+            ];
+          };
+        };
       };
     };
   };
